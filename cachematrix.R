0168cc63346aa68f3c5b96320115f2843c3b2ea3
CourseWorkH <CourseWorkH@users.noreply.github.com>
2014-06-22 08:55:25 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,76 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function caches a matrix
+## Call the makeCacheMatrix function to cash a matrix
+## This is done by using the 4 functions: set, get, setsolve, getsolve
+## The function returns a list of 4 functions
 
 makeCacheMatrix <- function(x = matrix()) {
 
+	# create a matrix to be cashed
+        
+	m <- NULL
+
+      set <- function(y) {
+
+	# environment is set for the matrix
+
+             x <<- y
+             m <<- NULL
+
+        }
+
+	 # set the functions to solve the cached matrix	
+	 # environments are set for the inverse matrix
+
+       get <- function() x
+
+       setsolve <- function(solve) m <<- solve
+
+       getsolve <- function() m
+
+	 # list with 4 functions is returned
+
+       list(set = set, get = get,
+
+             setsolve = setsolve,
+
+             getsolve = getsolve)
+
 }
 
-
-## Write a short comment describing this function
+# This functions calculates the inverse (solves) of a cashed matrix
+# The function first checks it the matrix has been cashed
+# If the matrix is cashed, this cashed matrix is used
+# If the matrix is not cashed, the solve function is performed 'as normal'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+	  #check if matrix is already been solved and cashed or not
+
+        m <- x$getsolve()
+
+	  #if matrix is cashed, this is retrieved
+
+        if(!is.null(m)) {
+
+                message("getting cached data")
+
+                return(m)
+
+        }
+
+	  #matrix is not cashed, calculate inverse with solve
+	  #first get the original matrix
+
+        data <- x$get()
+	  
+	  #solve the matrix
+
+        m <- solve(data, ...)
+
+        x$setsolve(m)
+
+	  #return inverse matrix
+
+        m
 }
+
